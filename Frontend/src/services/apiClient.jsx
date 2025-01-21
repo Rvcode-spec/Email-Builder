d29e1455cf410b7f0959b0cf20c9c8a556246733
DSA Code <civilstudy2024@gmail.com>
2025-01-22 04:42:28 +0530

    Update apiClient.jsx

--- Frontend/src/services/apiClient.jsx
+++ Frontend/src/services/apiClient.jsx
@@ -2,7 +2,7 @@
 import Axios from "axios";
 
 const apiClient = Axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://email-builder-app.onrender.com",
   headers: { "Content-Type": "application/json" },
 });
 
